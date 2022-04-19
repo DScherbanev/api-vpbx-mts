0ec2bad2412d42dbb83b4b242604335b610d4d95
Dmitry Scherbanev <scherbanev.d@yandex.ru>
2022-04-19 14:11:26 +0300

    Замена +7 на 8 в именах файлов

--- vpbx_api.py
+++ vpbx_api.py
@@ -11,23 +11,23 @@ VPBX_API_TOKEN = os.getenv('VPBX_API_TOKEN')
 CALL_HISTORY_ENDPOINT = 'https://vpbx.mts.ru/api/v1/callHistory/enterprise'
 CALL_REC_ENDPOINT = 'https://vpbx.mts.ru/api/callRecording/mp3/'
 PAGE_SIZE = 10
-DATE_FROM = '2022/01/01'
-DATE_TO = '2022/04/11'
+DATE_FROM = '01/01/2022'
+DATE_TO = '19/04/2022'
 
 
 def date_to_timestamp(date_time):
     """Преобразование даты в UNIX timestamp."""
-    return int(datetime.strptime(date_time, '%Y/%m/%d').timestamp()*1000)
+    return int(datetime.strptime(date_time, '%d/%m/%Y').timestamp()*1000)
 
 
 def timestamp_to_date(timestamp):
     """Преобразование UNIX timestamp в дату."""
-    return datetime.fromtimestamp(timestamp/1000).strftime('%Y-%m-%d')
+    return datetime.fromtimestamp(timestamp/1000).strftime('%d-%m-%Y')
 
 
 def timestamp_to_date_time(timestamp):
     """Преобразование UNIX timestamp в дату-время."""
-    return datetime.fromtimestamp(timestamp/1000).strftime('%Y-%m-%d_%H-%M-%S')
+    return datetime.fromtimestamp(timestamp/1000).strftime('%d-%m-%Y_%H-%M-%S')
 
 
 def get_api_response(page):
@@ -47,18 +47,18 @@ def get_api_response(page):
     return total_elements, total_pages, call_history
 
 
-def save_call_record_mp3(call_id, calling_num, called_num, call_time_ts):
+def save_call_record_mp3(call_id, calling_num, called_num, call_time_ts, call_duration):
     rec_params = {
         'X-AUTH-TOKEN': VPBX_API_TOKEN,
         'cache-control': 'no-cache'
     }
     response = requests.get(CALL_REC_ENDPOINT  + call_id, params=rec_params, allow_redirects=True)
     if response.status_code == 200:
-        filename = calling_num + '_'  + called_num + '_' + timestamp_to_date_time(call_time_ts) +'.mp3'
+        filename = calling_num + '_'  + called_num + '_' + timestamp_to_date_time(call_time_ts) + '_' + str(call_duration) + '.mp3'
         open(filename, 'wb').write(response.content)
-        print('Записан файл: ', filename)
-    else:
-        print('Код ответа:', response.status_code)
+        print('Записан файл:', filename)
+    # else:
+    #     print('Код ответа:', response.status_code)
 
 
 def main():
@@ -70,15 +70,12 @@ def main():
 
         for element in range(elements_on_page):
             call_id = call_history[element]['extTrackingId']
-            calling_num = call_history[element]['callingNumber']
-            called_num = call_history[element]['calledNumber']
+            calling_num = call_history[element]['callingNumber'].replace('+7', '8')
+            called_num = call_history[element]['calledNumber'].replace('+7', '8')
             call_time_ts = call_history[element]['callTime']
-
-            # Вывод информации по конкретному звонку
-            # pprint.pprint(call_history[element])
-
-            print(call_id, calling_num, called_num, timestamp_to_date_time(call_time_ts))
-            save_call_record_mp3(call_id, calling_num, called_num, call_time_ts)
+            call_duration = call_history[element]['duration']
+            # print(call_id, calling_num, called_num, timestamp_to_date_time(call_time_ts), call_duration)
+            save_call_record_mp3(call_id, calling_num, called_num, call_time_ts, call_duration)
 
 
 if __name__ == '__main__':
