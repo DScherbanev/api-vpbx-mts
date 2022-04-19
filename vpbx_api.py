7171877c9285bdd7ba2c88e9c64ffd8a9dab5820
Dmitry Scherbanev <scherbanev.d@yandex.ru>
2022-04-19 13:30:54 +0300

    Добавлен обход всех страниц истории звонков

--- vpbx_api.py
+++ vpbx_api.py
@@ -19,63 +19,66 @@ def date_to_timestamp(date_time):
     """Преобразование даты в UNIX timestamp."""
     return int(datetime.strptime(date_time, '%Y/%m/%d').timestamp()*1000)
 
+
 def timestamp_to_date(timestamp):
     """Преобразование UNIX timestamp в дату."""
     return datetime.fromtimestamp(timestamp/1000).strftime('%Y-%m-%d')
+
 
 def timestamp_to_date_time(timestamp):
     """Преобразование UNIX timestamp в дату-время."""
     return datetime.fromtimestamp(timestamp/1000).strftime('%Y-%m-%d_%H-%M-%S')
 
 
-def main():
+def get_api_response(page):
     params = {
         'status': 'PLACED',
         'dateFrom': date_to_timestamp(DATE_FROM),
         'dateTo': date_to_timestamp(DATE_TO),
-        'page': 0,
+        'page': page,
         'size': PAGE_SIZE,
         'X-AUTH-TOKEN': VPBX_API_TOKEN,
         'cache-control': 'no-cache'
     }
-
     response = requests.get(CALL_HISTORY_ENDPOINT, params=params)
     total_elements = response.json()['totalElements']
     total_pages = response.json()['totalPages']
     call_history = response.json()['content']
-    elements_on_page = len(call_history)
+    return total_elements, total_pages, call_history
 
 
-    print('totalElements', total_elements)
-    print('Pages', total_pages)
-    print('Elements on page', elements_on_page)
-
-    n = 3
-
-    call_id = call_history[n]['extTrackingId']
-    calling_num = call_history[n]['callingNumber']
-    called_num = call_history[n]['calledNumber']
-    call_time_ts = call_history[n]['callTime']
-
-    print(call_id, calling_num, called_num, timestamp_to_date_time(call_time_ts))
-
-
-    # Вывод информации по конкретному звонку
-    pprint.pprint(call_history[n])
-
+def save_call_record_mp3(call_id, calling_num, called_num, call_time_ts):
     rec_params = {
         'X-AUTH-TOKEN': VPBX_API_TOKEN,
         'cache-control': 'no-cache'
     }
-
     response = requests.get(CALL_REC_ENDPOINT  + call_id, params=rec_params, allow_redirects=True)
-
     if response.status_code == 200:
         filename = calling_num + '_'  + called_num + '_' + timestamp_to_date_time(call_time_ts) +'.mp3'
         open(filename, 'wb').write(response.content)
         print('Записан файл: ', filename)
     else:
         print('Код ответа:', response.status_code)
+
+
+def main():
+    total_elements, total_pages, call_history = get_api_response(0)
+
+    for page in range(total_pages):
+        call_history = get_api_response(page)[2]
+        elements_on_page = len(call_history)
+
+        for element in range(elements_on_page):
+            call_id = call_history[element]['extTrackingId']
+            calling_num = call_history[element]['callingNumber']
+            called_num = call_history[element]['calledNumber']
+            call_time_ts = call_history[element]['callTime']
+
+            # Вывод информации по конкретному звонку
+            # pprint.pprint(call_history[element])
+
+            print(call_id, calling_num, called_num, timestamp_to_date_time(call_time_ts))
+            save_call_record_mp3(call_id, calling_num, called_num, call_time_ts)
 
 
 if __name__ == '__main__':
